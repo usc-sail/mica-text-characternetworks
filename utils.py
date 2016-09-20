92084b9870929dbfbf8beb8f869e661677dec1b6
Victor Martinez <vrmp00@gmail.com>
2016-09-20 16:39:44 -0700

    Better adj matrix

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ def read(filename, threshold = 2):
     tree=minidom.parse(filename)
     mov=tree.getElementsByTagName('movie')[0]
 
-    movietitle=mov.getAttribute('title')
+    # movietitle=mov.getAttribute('title')
     dlgs=mov.getElementsByTagName('dialogue')
     lastSpeaker=''
 
@@ -36,31 +36,28 @@ def read(filename, threshold = 2):
 
                 lastSpeaker=thisSpeaker
 
-
-    for sp1 in spkTransition:
-        for sp2 in spkTransition[sp1]:
-            #make the graph more symmetric
-            if sp1<sp2:
-                spkTransition[sp1][sp2]+=spkTransition[sp2][sp1]
-                spkTransition[sp2][sp1]=spkTransition[sp1][sp2]/2
-                spkTransition[sp1][sp2]=spkTransition[sp1][sp2]/2
-
     char_list=np.array(sorted(list(spkTransition.keys()))) #Python 3
     N=len(char_list)
     adj=np.zeros((N,N))
 
-    # Make it more symetric (?)
-    for i,ch1 in enumerate(char_list):
-        for j,ch2 in enumerate(char_list):
-            adj[i][j]=-spkTransition[ch1][ch2]
+    for i, ch1 in enumerate(char_list):
+        for j, ch2 in enumerate(char_list):
+            if ch2 in spkTransition[ch1]:
+                adj[i][j] = spkTransition[ch1][ch2]
 
-    # Filter those with not so many dialogs
-    total_dlgs=np.sum(-adj,axis=1)
-    idx=np.array(np.where(total_dlgs>threshold)[0])
-    adj=adj[idx,:]
-    adj=adj[:,idx]
-    char_list=char_list[idx]
-    
+    # Make it symmetric
+    adj = (adj.transpose() + adj) / 2.0
+
+
+    # Filter those without many dialogs
+    total_dlgs = np.sum(adj, axis = 1)
+    idx = np.array(np.where(total_dlgs > threshold)[0])
+    adj = adj[idx, :][:, idx]
+    char_list = char_list[idx]
+
+    # Make it binary
+    adj = (adj > 0).astype(int)
+
     return (char_list, adj)
 
 def genders(filename):
