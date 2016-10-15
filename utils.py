fd69ba2740f9c563716c71d9d218094b2f3d469b
Victor Martinez <vrmp00@gmail.com>
2016-10-15 14:34:28 -0700

    Added data from master, improved utils:Adj

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-from xml.dom import minidom
+import untangle
 import sys,json, re
-from collections import defaultdict 
+from collections import defaultdict, Counter
 import numpy as np
 import networkx as nx
 import scipy.io 
@@ -10,51 +10,34 @@ import string
 punct_elim = str.maketrans({key:' ' for key in string.punctuation})
 
 def read(filename, threshold = 2):
-	spkTransition=defaultdict(lambda:defaultdict(int))
+	
+	doc = untangle.parse(filename)
+	movie_name = doc.movie['title']
 
+	transitions = []
+	for diag in doc.movie.dialogue:
+		speaker = diag.speaker[0].cdata 
 
-	tree=minidom.parse(filename)
-	mov=tree.getElementsByTagName('movie')[0]
+		for spk in diag.speaker[1:]:
+			transitions.append((speaker, spk.cdata))
+			speaker = spk.cdata 
 
-	movie_name=mov.getAttribute('title')
-	dlgs=mov.getElementsByTagName('dialogue')
-	lastSpeaker=''
+	# Filter empty speakers and same speaker
+	transitions = list(filter(lambda x: x[0]!=x[1], transitions))
+	transitions = list(filter(lambda x: len(x[0])>0 and len(x[1])>0, transitions))
 
-	for d in dlgs:
-		utts=d.getElementsByTagName('utterance')
-		spkrs=d.getElementsByTagName('speaker')
+	# 
+	weights = Counter(transitions)
+	char_list = np.array(sorted(list(set([y for x in map(lambda x: [x[0], x[1]], weights.keys())
+				   						    for y in x]))))
 
-		for s in spkrs:
-			if s.firstChild:
-				#dlgs_by_speaker[s.firstChild.data].append(u.firstChild.data)
-				thisSpeaker=s.firstChild.data
+	# Create adj matrix
+	adj = np.zeros((len(char_list), len(char_list)))
+	for i in range(len(char_list)):
+		for j in range(len(char_list)):
+			adj[i, j] = weights[(char_list[i], char_list[j])] > 0
 
-				if len(thisSpeaker.split())>2:
-					continue
-
-				if len(lastSpeaker)>0:
-					lastSpeaker=re.sub(' ','_',lastSpeaker)
-					thisSpeaker=re.sub(' ','_',thisSpeaker)
-					lastSpeaker=re.sub('[^_A-Za-z]','',lastSpeaker)
-					thisSpeaker=re.sub('[^_A-Za-z]','',thisSpeaker)
-					spkTransition[lastSpeaker][thisSpeaker]+=1
-
-				lastSpeaker=thisSpeaker
-
-	char_list=np.array(sorted(list(spkTransition.keys()))) #Python 3
-	N=len(char_list)
-	adj=np.zeros((N,N))
-
-	for i, ch1 in enumerate(char_list):
-		for j, ch2 in enumerate(char_list):
-			if ch2 in spkTransition[ch1]:
-				adj[i][j] = spkTransition[ch1][ch2]
-
-	# Make it symmetric
-	adj = (adj.transpose() + adj) / 2.0
-
-
-	# Filter those without many dialogs
+	# Filter those chars without many dialogs
 	total_dlgs = np.sum(adj, axis = 1)
 	idx = np.array(np.where(total_dlgs > threshold)[0])
 	adj = adj[idx, :][:, idx]
