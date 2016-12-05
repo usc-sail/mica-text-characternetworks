a59ea1a41709e41d21ba3a5f42772928265932cc
Victor <vrmp00@gmail.com>
2016-12-04 21:20:14 -0800

    looking for bug in VLIWC

--- LIWC/processLIWC.py
+++ LIWC/processLIWC.py
@@ -5,23 +5,25 @@ from os.path import basename
 vliwc = VLIWC(dict_file = "./LIWC2007_English131104.dic")
 headers = sorted(list(vliwc.catDict.values()))
 
-for script in glob("data/utterances_with_charnames/*.txt"):
-	with open("data/out/LIWC_"+basename(script).replace(".txt", ".tsv"), 'w') as outpt:
+for script in glob("../data/utterances_with_charnames/*.txt"):
+	with open("../data/LIWC_by_movie/LIWC_"+basename(script).replace(".txt", ".tsv"), 'w') as outpt:
 		
 		# Write headers
-		outpt.write("SPEAKER\t" + "\t".join(headers)+"\n")
+		outpt.write("SPEAKER\tNUM.WORDS\tWPS\t" + "\t".join(headers)+"\n")
 
 		with open(script) as inpt:
 			for line_num, line in enumerate(inpt):
 				try:
 					speaker, utt = line.strip().split(" => ")
+					speaker = speaker.replace("\n", " ")
 
 					measures = vliwc.processText(utt.strip())
 
-					outpt.write("{}\t{}\n".format(speaker, "\t".join([str(measures[h]) if h in measures else "0.0" for h in headers])))
+					outpt.write("{}\t{}\t{}\t{}\n".format(speaker, measures['num_words'], measures['wps'], "\t".join([str(measures[h]) if h in measures else "0.0" for h in headers])))
 
-				except:
+				except Exception as e:
 					print("File: {}\tLine: {} cannot be parsed".format(script, line_num))
+					print(e)
 					outpt.write("{}\t\n".format(speaker))
 
 	print("Done with {}".format(script))
