1c4474dec1e2742cd8b8d240030b7d593face5e0
Victor Martinez <vrmp00@gmail.com>
2017-01-22 13:34:57 -0800

    Mod liwc metrics

--- LIWC/processLIWC.py
+++ LIWC/processLIWC.py
@@ -1,11 +1,10 @@
 from VLIWC import VLIWC
 from glob import iglob as glob 
 from os.path import basename
+from collections import defaultdict
+from joblib import delayed, Parallel
 
-vliwc = VLIWC(dict_file = "./LIWC2007_English131104.dic")
-headers = sorted(list(vliwc.catDict.values()))
-
-for script in glob("../data/utterances_with_charnames/*.txt"):
+def processFile(script, vliwc, zero_dict, headers):
 	with open("../data/LIWC_by_movie/LIWC_"+basename(script).replace(".txt", ".tsv"), 'w') as outpt:
 		
 		# Write headers
@@ -14,17 +13,27 @@ for script in glob("../data/utterances_with_charnames/*.txt"):
 		with open(script) as inpt:
 			for line_num, line in enumerate(inpt):
 				try:
-					speaker, utt = line.strip().split(" => ")
-					speaker = speaker.replace("\n", " ")
+					speaker, utt = line.strip().split("=>")
+					speaker = speaker.replace("\n", " ").strip()
+					utt = utt.strip()
 
-					measures = vliwc.processText(utt.strip())
+					if len(utt) > 0:
+						measures = vliwc.processText(utt.strip())
+					else:
+						measures = zero_dict
 
-					outpt.write("{}\t{}\t{}\t{}\n".format(speaker, measures['num_words'], measures['wps'], "\t".join([str(measures[h]) if h in measures else "0.0" for h in headers])))
 
 				except Exception as e:
 					print("File: {}\tLine: {} cannot be parsed".format(script, line_num))
-					print(e)
-					outpt.write("{}\t\n".format(speaker))
+					measures = zero_dict
+
+				outpt.write("{}\t{}\t{}\t{}\n".format(speaker, measures['num_words'], measures['wps'], "\t".join([str(measures[h]) if h in measures else "0.0" for h in headers])))
 
 	print("Done with {}".format(script))
 
+if __name__ == '__main__':
+	zero_dict = defaultdict(int)
+	vliwc = VLIWC(dict_file = "./LIWC2007_English131104.dic")
+	headers = sorted(list(vliwc.catDict.values()))
+	Parallel(n_jobs = 4, verbose = 1)(delayed(processFile)(script, vliwc, zero_dict, headers) for script in glob("../data/utterances_with_charnames/*.txt"))
+	
